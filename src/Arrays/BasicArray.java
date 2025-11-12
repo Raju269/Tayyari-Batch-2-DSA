cdb6a2966b0beab190a614552b8934713cb92f83
Raju kumar <rajuk631149@gmail.com>
2025-11-12 13:31:22 +0530

    1st program

--- src/Arrays/BasicArray.java
+++
@@ -1,7 +0,0 @@
-package Arrays;
-
-public class BasicArray {
-    public static void main(String[] args) {
-        System.out.println("Hello world ");
-    }
-}
---
+++ src/Basic/FirstProgram.java
@@ -0,0 +1,8 @@
+package Basic;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello ji  ");
+        System.out.println("Raju kumar bhai kaise hai   ");
+    }
+}
